547d4fb1873b3461821f463a0f2ceda87d902e44
Wesley <wesley_ccb@hotmail.com>
2021-02-28 18:26:07 -0400

    atualizando

--- customers/add.php
+++ customers/add.php
@@ -12,44 +12,44 @@
   <div class="row">
     <div class="form-group col-md-7">
       <label for="name">Nome Completo</label>
-      <input type="text" class="form-control" name="customer['name']">
+      <input type="text" required class="form-control" name="customer['name']">
     </div>
 
     <div class="form-group col-md-3">
       <label for="campo3">Matrícula</label>
-      <input type="text" class="form-control" name="customer['ie']">
+      <input type="text" required class="form-control" name="customer['ie']">
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo2">Unidade</label>
-      <input type="text" class="form-control" name="customer['hood']">
+      <input type="text" required class="form-control" name="customer['hood']">
     </div>
   </div>
   
   <div class="row">
     <div class="form-group col-md-4">
       <label for="campo1">Município</label>
-      <input type="text" class="form-control" name="customer['city']">
+      <input type="text" required class="form-control" name="customer['city']">
     </div>
 
     <div class="form-group col-md-1">
       <label for="campo3">UF</label>
-      <input type="text" class="form-control" name="customer['state']">
+      <input type="text" required class="form-control" name="customer['state']">
     </div>
     
     <div class="form-group col-md-3">
       <label for="campo2">Telefone</label>
-      <input type="text" class="form-control" name="customer['mobile']">
+      <input type="text" required class="form-control" name="customer['mobile']">
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Data de Inclusão</label>
-      <input type="text" class="form-control" name="customer['date_inclu']">
+      <input type="text" required class="form-control" name="customer['date_inclu']">
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Data de Nascimento</label>
-      <input type="text" class="form-control" name="customer['birthdate']">
+      <input type="text" required class="form-control" name="customer['birthdate']">
     </div>
   </div>
   
